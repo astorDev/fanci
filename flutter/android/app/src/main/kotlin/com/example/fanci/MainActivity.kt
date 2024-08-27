bac9dfdabd99eb44cc2a594060a79055ff6c9449
Egor Tarasov <pavluchenk0@yandex.ru>
2024-08-27 14:22:42 +0400

    init flutter app (#1)

---
+++ flutter/android/app/src/main/kotlin/com/example/fanci/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.fanci
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ flutter/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fanci Playground',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Fanci Playground'),
+        ),
+        body: Center(
+          child: Text('Hello, Fanci!'),
+        ),
+      )
+    );
+  }
+}
