f48972c71da7c32ad4fd7826af00e53c8f218743
Egor Tarasov <pavluchenk0@yandex.ru>
2024-10-22 20:02:04 +0400

    styled playground and advanced playground fundament

--- flutter/lib/dropmenu/playground.dart
+++ flutter/lib/dropmenu/playground.dart
@@ -12,30 +12,19 @@ class DropdownPlaygroundApp extends StatelessWidget {
       appName: 'Dropdown',
       tabs: [
         PlaygroundTab(
+          emoji: '⚪',
+          label: 'Basic',
+          page: BasicPlayground(),
+        ),
+        PlaygroundTab(
           emoji: '🎨',
           label: 'Styled',
           page: StyledPlayground(),
         ),
         PlaygroundTab(
-          emoji: '⚪',
-          label: 'Basic',
-          page: BasicPlayground(),
-        ),
-      ],
-    );
-  }
-}
-
-class StyledPlayground extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        DropdownMenu(
-          initialSelection: currencies[0],
-          dropdownMenuEntries: currencies.map(toDropdownMenuEntry).toList(),
-          inputDecorationTheme: InputDecorationTheme(),
+          emoji: '🔬',
+          label: 'Advanced',
+          page: AdvancedPlayground(),
         ),
       ],
     );
@@ -71,6 +60,92 @@ class _BasicPlaygroundState extends State<BasicPlayground> {
             return Text('Picker: ${value.text}');
           }
         )
+      ],
+    );
+  }
+}
+
+class StyledPlayground extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        DropdownMenu(
+          initialSelection: currencies[0],
+          dropdownMenuEntries: currencies.map(toDropdownMenuEntry).toList(),
+          inputDecorationTheme: InputDecorationTheme()
+        ),
+        SizedBox(height: 50),
+        DropdownMenu(
+          initialSelection: currencies[0],
+          menuHeight: 200,
+          dropdownMenuEntries: currencies.map(toDropdownMenuEntry).toList(),
+          trailingIcon: Icon(
+            Icons.keyboard_arrow_down_rounded,
+            size: 20,
+          ),
+          selectedTrailingIcon: Icon(
+            Icons.keyboard_arrow_up_sharp,
+            size: 20
+          ),
+          inputDecorationTheme: InputDecorationTheme(
+            filled: true,
+            fillColor: Theme.of(context).colorScheme.onSurface.withOpacity(0.03),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                color: Theme.of(context).colorScheme.onSurface.withOpacity(0.1),
+                width: 0.6
+              ),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                color: Theme.of(context).colorScheme.primary.withOpacity(1),
+                width: 0.6
+              ),
+            )
+          )
+        ),
+      ],
+    );
+  }
+}
+
+class AdvancedPlayground extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        DropdownMenu(
+          initialSelection: currencies[0],
+          menuHeight: 200,
+          dropdownMenuEntries: currencies.map(toDropdownMenuEntry).toList(),
+          trailingIcon: Icon(
+            Icons.keyboard_arrow_down_rounded,
+            size: 20,
+          ),
+          selectedTrailingIcon: Icon(
+            Icons.keyboard_arrow_up_sharp,
+            size: 20
+          ),
+          inputDecorationTheme: InputDecorationTheme(
+            filled: true,
+            fillColor: Theme.of(context).colorScheme.onSurface.withOpacity(0.03),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                color: Theme.of(context).colorScheme.onSurface.withOpacity(0.1),
+                width: 0.6
+              ),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                color: Theme.of(context).colorScheme.primary.withOpacity(1),
+                width: 0.6
+              ),
+            )
+          )
+        ),
       ],
     );
   }
