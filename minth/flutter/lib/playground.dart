0d5d37e3d4227016f839711389720f354bd8491a
Egor Tarasov <pavluchenk0@yandex.ru>
2024-11-08 00:17:41 +0400

    minth web app pollished (#33)

--- minth/flutter/lib/playground.dart
+++ minth/flutter/lib/playground.dart
@@ -1,5 +1,6 @@
 import 'package:fanci/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:minth/dropdown.dart';
 import 'package:minth/theme.dart';
 
@@ -13,36 +14,43 @@ class MinthPlaygroundApp extends StatelessWidget {
     var lt = ThemeData.from(
       colorScheme: ColorScheme.fromSeed(
         seedColor: Colors.white,
-        dynamicSchemeVariant: DynamicSchemeVariant.monochrome
+        dynamicSchemeVariant: DynamicSchemeVariant.monochrome,
+        surface: Colors.white,
       )
     );
 
+    lt = lt.copyWith(
+      appBarTheme: AppBarTheme(
+        systemOverlayStyle: SystemUiOverlayStyle(
+          statusBarIconBrightness: Brightness.dark,
+          statusBarBrightness: Brightness.light,
+        ),
+      ),
+    );
 
     return PlaygroundApp(
-      appName: 'Minth', 
+      appName: 'Minth3', 
       theme: lt.copyWithMinthAdjustments(),
       darkTheme: ThemeData.dark().copyWithMinthAdjustments(),
       tabs: [
         PlaygroundTab(
           emoji: '🎨',
           label: 'Dropdown',
-          page: SafeArea(
-            child: Center(
-              child: SizedBox(
-                width: 120,
-                child: ThemedDropdownMenu(
-                  initialSelection: 'AED',
-                  dropdownMenuEntries: [
-                    DropdownMenuEntry(value: 'USD', label: 'USD (United States Dollar)'),
-                    DropdownMenuEntry(value: 'AED', label: 'AED (United Arab Emirates Dirham)'),
-                    DropdownMenuEntry(value: 'C', label: 'CCCCCCCCCC'),
-                    DropdownMenuEntry(value: 'D', label: 'DDDDDDDDDD'),
-                    DropdownMenuEntry(value: 'E', label: 'EEEEEEEEEE'),
-                    DropdownMenuEntry(value: 'F', label: 'FFFFFFFFFF'),
-                  ],
-                ),
-              )
-            ),
+          page: Center(
+            child: SizedBox(
+              width: 120,
+              child: ThemedDropdownMenu(
+                initialSelection: 'AED',
+                dropdownMenuEntries: [
+                  DropdownMenuEntry(value: 'USD', label: 'USD (United States Dollar)'),
+                  DropdownMenuEntry(value: 'AED', label: 'AED (United Arab Emirates Dirham)'),
+                  DropdownMenuEntry(value: 'C', label: 'CCCCCCCCCC'),
+                  DropdownMenuEntry(value: 'D', label: 'DDDDDDDDDD'),
+                  DropdownMenuEntry(value: 'E', label: 'EEEEEEEEEE'),
+                  DropdownMenuEntry(value: 'F', label: 'FFFFFFFFFF'),
+                ],
+              ),
+            )
           )
         )
       ]
