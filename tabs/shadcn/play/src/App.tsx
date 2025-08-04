0df02973c16d7a3c4779045b30cd9ac6d86138fb
Egor Tarasov <vosarat1995@google.com>
2025-08-04 19:18:59 +0400

    shadcn tabs example updated

--- tabs/shadcn/play/src/App.tsx
+++ tabs/shadcn/play/src/App.tsx
@@ -1,14 +1,58 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { Textarea } from "./components/ui/textarea"
+import { Button } from "./components/ui/button"
+import { Input } from "./components/ui/input"
+import { Label } from "./components/ui/label"
 
 function App() {
   return (
-    <Tabs defaultValue="account" className="w-[400px]">
-      <TabsList>
-        <TabsTrigger value="account">Account</TabsTrigger>
-        <TabsTrigger value="password">Password</TabsTrigger>
+    <Tabs defaultValue="account" className=""  >
+      <TabsList className="w-full">
+        <TabsTrigger value="account">Configuration</TabsTrigger>
+        <TabsTrigger value="password">Schema</TabsTrigger>
       </TabsList>
-      <TabsContent value="account">Make changes to your account here.</TabsContent>
-      <TabsContent value="password">Change your password here.</TabsContent>
+      <TabsContent value="account" className="flex flex-col">
+        <p className="text-xs text-muted-foreground my-2">
+          💡 If you are non-programmer, maybe use the Visual Editor
+        </p>
+        <Tabs defaultValue="json" className="flex-1">
+          <TabsList className="w-full">
+            <TabsTrigger value="json">JSON Editor</TabsTrigger>
+            <TabsTrigger value="form">Visual Editor</TabsTrigger>
+          </TabsList>
+          <TabsContent className="flex flex-col" value="json">
+            <Textarea 
+              className="h-[240px]" 
+              placeholder="Enter JSON configuration here"
+              value={`{
+      "name": "shadcn",
+      "description": "A shadcn component"
+}`}
+            />
+            <Button disabled className="mt-4">
+              Save
+            </Button>
+          </TabsContent>
+          <TabsContent className="flex flex-col" value="form">
+            <div className="py-4 h-[240px]">
+            <form >
+                <div className="grid w-full max-w-sm items-center gap-3">
+                  <Label htmlFor="email">Name</Label>
+                  <Input type="text" id="name" placeholder="Name" />
+
+                  <Label htmlFor="email">Description</Label>
+                  <Input type="email" id="email" placeholder="Description" />
+                </div>
+            </form>
+
+            </div>
+              <Button disabled className="mt-4">
+                Save
+              </Button>
+          </TabsContent>
+        </Tabs>
+      </TabsContent>
+      <TabsContent value="password">Here should be editor for schema json</TabsContent>
     </Tabs>
   )
 }
--- tabs/shadcn/play/src/main.tsx
+++ tabs/shadcn/play/src/main.tsx
@@ -6,8 +6,8 @@ import { Card } from './components/ui/card.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Card className='fixed inset-0 m-auto 
-      w-[420px] h-[360px] px-4'>
+    <Card className='fixed left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2
+      w-[420px] min-h-[360px] px-4'>
       <App />
     </Card>
   </StrictMode>,
