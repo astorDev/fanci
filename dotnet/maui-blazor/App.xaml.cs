db54ac504273d7b2e1b2aeff656fe6f15f0357ac
Egor Tarasov <vosarat1995+1@gmail.com>
2026-01-06 18:04:43 +0100

    Playground of maui-blazor (#47)
    Co-authored-by: Egor Tarasov <vosarat1995@google.com>

---
+++ dotnet/maui-blazor/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace Fanci.Playground.MauiBlazor;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+	}
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new MainPage()) { Title = "Fanci.Playground.MauiBlazor" };
+	}
+}
